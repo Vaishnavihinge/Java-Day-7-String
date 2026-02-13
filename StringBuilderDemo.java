c3a195d65f9095a7befba69f3412f5398c1a9d1e
Vaishnavihinge <117362673+Vaishnavihinge@users.noreply.github.com>
2026-02-13 18:45:00 +0530

    Add files via upload

---
+++ StringBuilderDemo.java
@@ -0,0 +1,23 @@
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Hello"); //creating object
+
+       //Methods of String Builder
+
+        sb.append(" World");
+        System.out.println("After append: " + sb);
+
+        sb.insert(5, " Java");
+        System.out.println("After insert: " + sb);
+
+        sb.replace(0, 5, "Hi");
+        System.out.println("After replace: " + sb);
+
+        sb.delete(2, 7);
+        System.out.println("After delete: " + sb);
+
+        sb.reverse();
+        System.out.println("After reverse: " + sb);
+    }
+}
